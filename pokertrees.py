00dc2523f8362c4adccfd94f613d22bccef8e7a7
Wesley Tansey <wes.tansey@gmail.com>
2013-03-19 23:50:44 -0500

    Added betting history string to nodes

--- pokertrees.py
+++ pokertrees.py
@@ -65,7 +65,7 @@ class GameTree(object):
                 next_player = (next_player + 1) % self.players
         holes = [[]] * self.players
         board = ()
-        self.root = HolecardChanceNode(None, committed, holes, board, self.deck, self.holecards)
+        self.root = HolecardChanceNode(None, committed, holes, board, self.deck, "", self.holecards)
         # Deal holecards
         all_hc = self.deal_holecards()
         # Create a child node for every possible distribution
@@ -75,58 +75,59 @@ class GameTree(object):
             for c in cur_holes:
                 f = f + c
             cur_deck = filter(lambda x: not (x in f), self.deck)
-            self.build_rounds(self.root, players_in, committed, cur_holes, board, cur_deck, 0, bets, next_player)
+            self.build_rounds(self.root, players_in, committed, cur_holes, board, cur_deck, "", 0, bets, next_player)
 
     def deal_holecards(self):
         a = combinations(self.deck, self.holecards)
         return filter(lambda x: all_unique(x), permutations(a, self.players))
 
-    def build_rounds(self, root, players_in, committed, holes, board, deck, round_idx, bets = None, next_player = 0):
+    def build_rounds(self, root, players_in, committed, holes, board, deck, bet_history, round_idx, bets = None, next_player = 0):
         if round_idx == len(self.roundinfo):
-            self.showdown(root, players_in, committed, holes, board, deck)
+            self.showdown(root, players_in, committed, holes, board, deck, bet_history)
             return
+        bet_history += "/"
         cur_round = self.roundinfo[round_idx]
         while not players_in[next_player]:
             next_player = (next_player + 1) % self.players
         if cur_round.boardcards:
-            self.deal_boardcards(root, next_player, players_in, committed, holes, board, deck, round_idx)
+            self.deal_boardcards(root, next_player, players_in, committed, holes, board, deck, bet_history, round_idx)
         else:
             if bets is None:
                 bets = [0] * self.players
-            self.build_bets(root, next_player, players_in, committed, holes, board, deck, round_idx, players_in.count(True), 0, bets)
+            self.build_bets(root, next_player, players_in, committed, holes, board, deck, bet_history, round_idx, players_in.count(True), 0, bets)
 
-    def deal_boardcards(self, root, next_player, players_in, committed, holes, board, deck, round_idx):
+    def deal_boardcards(self, root, next_player, players_in, committed, holes, board, deck, bet_history, round_idx):
         cur_round = self.roundinfo[round_idx]
-        bnode = BoardcardChanceNode(root, committed, holes, board, deck, cur_round.boardcards)
+        bnode = BoardcardChanceNode(root, committed, holes, board, deck, bet_history, cur_round.boardcards)
         all_bc = combinations(deck, cur_round.boardcards)
         for bc in all_bc:
             cur_board = board + bc
             cur_deck = filter(lambda x: not (x in bc), deck)
-            self.build_bets(bnode, next_player, players_in, committed, holes, cur_board, cur_deck, round_idx, players_in.count(True), 0, [0] * self.players)
+            self.build_bets(bnode, next_player, players_in, committed, holes, cur_board, cur_deck, bet_history, round_idx, players_in.count(True), 0, [0] * self.players)
 
-    def build_bets(self, root, next_player, players_in, committed, holes, board, deck, round_idx, min_actions_this_round, actions_this_round, bets_this_round):
+    def build_bets(self, root, next_player, players_in, committed, holes, board, deck, bet_history, round_idx, min_actions_this_round, actions_this_round, bets_this_round):
         # if everyone else folded, end the hand
         if players_in.count(True) == 1:
-            self.showdown(root, players_in, committed, holes, board, deck)
+            self.showdown(root, players_in, committed, holes, board, deck, bet_history)
             return
         # if everyone checked or the last raisor has been called, end the round
         if actions_this_round >= min_actions_this_round and self.all_called_last_raisor_or_folded(players_in, bets_this_round):
-            self.build_rounds(root, players_in, committed, holes, board, deck, round_idx + 1)
+            self.build_rounds(root, players_in, committed, holes, board, deck, bet_history, round_idx + 1)
             return
         cur_round = self.roundinfo[round_idx]
-        anode = ActionNode(root, committed, holes, board, deck, next_player)
+        anode = ActionNode(root, committed, holes, board, deck, bet_history, next_player)
         # get the next player to act
         next_player = (next_player + 1) % self.players
         while not players_in[next_player]:
             next_player = (next_player + 1) % self.players
         # add a folding option if someone has bet more than this player
         if committed[anode.player] < max(committed):
-            self.add_fold_child(anode, next_player, players_in, committed, holes, board, deck, round_idx, min_actions_this_round, actions_this_round, bets_this_round)
+            self.add_fold_child(anode, next_player, players_in, committed, holes, board, deck, bet_history, round_idx, min_actions_this_round, actions_this_round, bets_this_round)
         # add a calling/checking option
-        self.add_call_child(anode, next_player, players_in, committed, holes, board, deck, round_idx, min_actions_this_round, actions_this_round, bets_this_round)
+        self.add_call_child(anode, next_player, players_in, committed, holes, board, deck, bet_history, round_idx, min_actions_this_round, actions_this_round, bets_this_round)
         # add a raising option if this player has not reached their max bet level
         if cur_round.maxbets[anode.player] > max(bets_this_round):
-            self.add_raise_child(anode, next_player, players_in, committed, holes, board, deck, round_idx, min_actions_this_round, actions_this_round, bets_this_round)
+            self.add_raise_child(anode, next_player, players_in, committed, holes, board, deck, bet_history, round_idx, min_actions_this_round, actions_this_round, bets_this_round)
 
     def all_called_last_raisor_or_folded(self, players_in, bets):
         betlevel = max(bets)
@@ -135,32 +136,36 @@ class GameTree(object):
                 return False
         return True
 
-    def add_fold_child(self, root, next_player, players_in, committed, holes, board, deck, round_idx, min_actions_this_round, actions_this_round, bets_this_round):
+    def add_fold_child(self, root, next_player, players_in, committed, holes, board, deck, bet_history, round_idx, min_actions_this_round, actions_this_round, bets_this_round):
         players_in[root.player] = False
-        self.build_bets(root, next_player, players_in, committed, holes, board, deck, round_idx, min_actions_this_round, actions_this_round + 1, bets_this_round)
+        bet_history += 'f'
+        self.build_bets(root, next_player, players_in, committed, holes, board, deck, bet_history, round_idx, min_actions_this_round, actions_this_round + 1, bets_this_round)
+        # TODO: root.fold_action = root.children[-1]
         players_in[root.player] = True
 
-    def add_call_child(self, root, next_player, players_in, committed, holes, board, deck, round_idx, min_actions_this_round, actions_this_round, bets_this_round):
+    def add_call_child(self, root, next_player, players_in, committed, holes, board, deck, bet_history, round_idx, min_actions_this_round, actions_this_round, bets_this_round):
         player_commit = committed[root.player]
         player_bets = bets_this_round[root.player]
         committed[root.player] = max(committed)
         bets_this_round[root.player] = max(bets_this_round)
+        bet_history += 'c'
         #print "In: {0} Committed: {1} Holes: {2} Board: {3} Deck: {4} Round: {5} MinActions: {6} CurActions: {7} BetsThisRound: {8}".format(players_in, committed, holes, board, deck, round_idx, min_actions_this_round, actions_this_round + 1, bets_this_round)
-        self.build_bets(root, next_player, players_in, committed, holes, board, deck, round_idx, min_actions_this_round, actions_this_round + 1, bets_this_round)
+        self.build_bets(root, next_player, players_in, committed, holes, board, deck, bet_history, round_idx, min_actions_this_round, actions_this_round + 1, bets_this_round)
         committed[root.player] = player_commit
         bets_this_round[root.player] = player_bets
 
-    def add_raise_child(self, root, next_player, players_in, committed, holes, board, deck, round_idx, min_actions_this_round, actions_this_round, bets_this_round):
+    def add_raise_child(self, root, next_player, players_in, committed, holes, board, deck, bet_history, round_idx, min_actions_this_round, actions_this_round, bets_this_round):
         cur_round = self.roundinfo[round_idx]
         prev_betlevel = bets_this_round[root.player]
         prev_commit = committed[root.player]
         bets_this_round[root.player] = max(bets_this_round) + 1
         committed[root.player] += (bets_this_round[root.player] - prev_betlevel) * cur_round.betsize
-        self.build_bets(root, next_player, players_in, committed, holes, board, deck, round_idx, min_actions_this_round, actions_this_round + 1, bets_this_round)
+        bet_history += 'r'
+        self.build_bets(root, next_player, players_in, committed, holes, board, deck, bet_history, round_idx, min_actions_this_round, actions_this_round + 1, bets_this_round)
         bets_this_round[root.player] = prev_betlevel
         committed[root.player] = prev_commit
 
-    def showdown(self, root, players_in, committed, holes, board, deck):
+    def showdown(self, root, players_in, committed, holes, board, deck, bet_history):
         if players_in.count(True) == 1:
             winners = [i for i,v in enumerate(players_in) if v]
         else:
@@ -188,7 +193,7 @@ class GameTree(object):
         #    print "Pot: {0} Payoff: {1} Winners: {2} Holes: {3} Board: {4}".format(pot, payoff, winners, holes, board)
         for w in winners:
             payoffs[w] += payoff
-        TerminalNode(root, committed, holes, board, deck, payoffs)
+        TerminalNode(root, committed, holes, board, deck, bet_history, payoffs)
 
     def holecard_distributions(self):
         x = Counter(combinations(self.deck, self.holecards))
@@ -196,11 +201,12 @@ class GameTree(object):
         return zip(x.keys(),[y / d for y in x.values()])
 
 class Node(object):
-    def __init__(self, parent, committed, holecards, board, deck):
+    def __init__(self, parent, committed, holecards, board, deck, bet_history):
         self.committed = deepcopy(committed)
         self.holecards = deepcopy(holecards)
         self.board = deepcopy(board)
         self.deck = deepcopy(deck)
+        self.bet_history = deepcopy(bet_history)
         if parent:
             self.parent = parent
             self.parent.add_child(self)
@@ -212,35 +218,35 @@ class Node(object):
             self.children.append(child)
 
 class TerminalNode(Node):
-    def __init__(self, parent, committed, holecards, board, deck, payoffs):
-        Node.__init__(self, parent, committed, holecards, board, deck)
+    def __init__(self, parent, committed, holecards, board, deck, bet_history, payoffs):
+        Node.__init__(self, parent, committed, holecards, board, deck, bet_history)
         self.payoffs = payoffs
 
 class HolecardChanceNode(Node):
-    def __init__(self, parent, committed, holecards, board, deck, todeal):
-        Node.__init__(self, parent, committed, holecards, board, deck)
+    def __init__(self, parent, committed, holecards, board, deck, bet_history, todeal):
+        Node.__init__(self, parent, committed, holecards, board, deck, bet_history)
         self.todeal = todeal
         self.children = []
 
 class BoardcardChanceNode(Node):
-    def __init__(self, parent, committed, holecards, board, deck, todeal):
-        Node.__init__(self, parent, committed, holecards, board, deck)
+    def __init__(self, parent, committed, holecards, board, deck, bet_history, todeal):
+        Node.__init__(self, parent, committed, holecards, board, deck, bet_history)
         self.todeal = todeal
-        self.children = []        
-
-class ActionNode(Node):
-    def __init__(self, parent, committed, holecards, board, deck, player):
-        Node.__init__(self, parent, committed, holecards, board, deck)
-        self.player = player
         self.children = []
 
-class PlayerTree(GameTree):
+class ActionNode(Node):
+    def __init__(self, parent, committed, holecards, board, deck, bet_history, player):
+        Node.__init__(self, parent, committed, holecards, board, deck, bet_history)
+        self.player = player
+        self.children = []
+        self.raise_action = None
+        self.call_action = None
+        self.fold_action = None
+
+class PlayerView(GameTree):
     def __init__(self, player, players, deck, holecards, rounds, ante, blinds, history_format = '{holecards}:{boardcards}:{bets}'):
         GameTree.__init__(self, players, deck, holecards, rounds, ante, blinds, history_format)
         self.player = player
-
-    def build(self):
-        pass
 
 class OpponentNode(Node):
     def __init__(self, parent, opponent):
--- test_gametree.py
+++ test_gametree.py
@@ -16,38 +16,52 @@ assert(len(tree.root.children[0].children) == 2)
 # /f
 assert(type(tree.root.children[0].children[0]) == TerminalNode)
 assert(tree.root.children[0].children[0].payoffs == [-2,2])
+assert(tree.root.children[0].children[0].bet_history == '/f')
 # /c
 assert(type(tree.root.children[0].children[1]) == ActionNode)
+assert(tree.root.children[0].children[1].bet_history == '/c')
 assert(len(tree.root.children[0].children[1].children) == 1)
-# /cc
 assert(tree.root.children[0].children[1].player == 1)
+# /cc/[boardcard]
 assert(type(tree.root.children[0].children[1].children[0]) == BoardcardChanceNode)
+assert(tree.root.children[0].children[1].children[0].bet_history == '/cc/')
 assert(len(tree.root.children[0].children[1].children[0].children) == 2)
+# /cc/[action]
 assert(type(tree.root.children[0].children[1].children[0].children[0]) == ActionNode)
+assert(tree.root.children[0].children[1].children[0].children[0].bet_history == '/cc/')
 assert(len(tree.root.children[0].children[1].children[0].children[0].children) == 2)
-# /cc/r
 assert(tree.root.children[0].children[1].children[0].children[0].player == 0)
+# /cc/r
 assert(type(tree.root.children[0].children[1].children[0].children[0].children[1]) == ActionNode)
-assert(tree.root.children[0].children[1].children[0].children[0].children[1].player == 1)
+assert(tree.root.children[0].children[1].children[0].children[0].children[1].bet_history == '/cc/r')
 assert(len(tree.root.children[0].children[1].children[0].children[0].children[1].children) == 3)
+assert(tree.root.children[0].children[1].children[0].children[0].children[1].player == 1)
 # /cc/c
 assert(type(tree.root.children[0].children[1].children[0].children[0].children[0]) == ActionNode)
-assert(tree.root.children[0].children[1].children[0].children[0].children[0].player == 1)
+assert(tree.root.children[0].children[1].children[0].children[0].children[0].bet_history == '/cc/c')
 assert(len(tree.root.children[0].children[1].children[0].children[0].children[0].children) == 2)
+assert(tree.root.children[0].children[1].children[0].children[0].children[0].player == 1)
 # /cc/cc
 assert(type(tree.root.children[0].children[1].children[0].children[0].children[0].children[0]) == TerminalNode)
+assert(tree.root.children[0].children[1].children[0].children[0].children[0].children[0].bet_history == '/cc/cc')
 assert(tree.root.children[0].children[1].children[0].children[0].children[0].children[0].payoffs == [-3,3])
 # /cc/cr
 assert(type(tree.root.children[0].children[1].children[0].children[0].children[0].children[1]) == ActionNode)
+assert(tree.root.children[0].children[1].children[0].children[0].children[0].children[1].bet_history == '/cc/cr')
 assert(len(tree.root.children[0].children[1].children[0].children[0].children[0].children[1].children) == 3)
+assert(tree.root.children[0].children[1].children[0].children[0].children[0].children[1].player == 0)
 # /cc/crr
 assert(type(tree.root.children[0].children[1].children[0].children[0].children[0].children[1].children[2]) == ActionNode)
+assert(tree.root.children[0].children[1].children[0].children[0].children[0].children[1].children[2].bet_history == '/cc/crr')
 assert(len(tree.root.children[0].children[1].children[0].children[0].children[0].children[1].children[2].children) == 2)
+assert(tree.root.children[0].children[1].children[0].children[0].children[0].children[1].children[2].player == 1)
 # /cc/crrf
 assert(type(tree.root.children[0].children[1].children[0].children[0].children[0].children[1].children[2].children[0]) == TerminalNode)
+assert(tree.root.children[0].children[1].children[0].children[0].children[0].children[1].children[2].children[0].bet_history == '/cc/crrf')
 assert(tree.root.children[0].children[1].children[0].children[0].children[0].children[1].children[2].children[0].payoffs == [5,-5])
 # /cc/crrc
 assert(type(tree.root.children[0].children[1].children[0].children[0].children[0].children[1].children[2].children[1]) == TerminalNode)
+assert(tree.root.children[0].children[1].children[0].children[0].children[0].children[1].children[2].children[1].bet_history == '/cc/crrc')
 assert(tree.root.children[0].children[1].children[0].children[0].children[0].children[1].children[2].children[1].payoffs == [-7,7])
 
 print "All passed!"
