038cab70173d43420bb14ac8e12ed29c10bb5b93
Wesley Tansey <wes.tansey@gmail.com>
2013-03-21 12:55:19 -0500

    Added EV calculation for a strategy profile. Added random strategy for leduc.

--- pokerstrategy.py
+++ pokerstrategy.py
@@ -30,14 +30,14 @@ class Strategy(object):
             tokens = line.split(' ')
             assert(len(tokens) == 4)
             key = tokens[0]
-            probs = [float(x) for x in tokens[1:]]
+            probs = [float(x) for x in reversed(tokens[1:])]
             self.policy[key] = probs
 
     def save_to_file(self, filename):
         f = open(filename, 'w')
         for key in sorted(self.policy.keys()):
             val = self.policy[key]
-            f.write("{0} {1:.9f} {2:.9f} {3:.9f}\n".format(key, val[0], val[1], val[2]))
+            f.write("{0} {1:.9f} {2:.9f} {3:.9f}\n".format(key, val[2], val[1], val[0]))
         f.flush()
         f.close()
 
@@ -46,3 +46,40 @@ class StrategyProfile(object):
         assert(gametree.players == len(strategies))
         self.gametree = gametree
         self.strategies = strategies
+
+    def expected_value(self):
+        return self.ev_helper(self.gametree.root, 1)
+
+    def ev_helper(self, root, pathprob, depth = 0):
+        if type(root) is TerminalNode:
+            payoffs = [x*pathprob for x in root.payoffs]
+            #print '{0}Probpath: {1:.9f} Raw Payoffs: {2} Weighted: {3}'.format('\t'*depth, pathprob, root.payoffs, [float("{0:.4f}".format(x)) for x in payoffs])
+            return payoffs
+        if type(root) is HolecardChanceNode or type(root) is BoardcardChanceNode:
+            payoffs = [0] * self.gametree.players
+            prob = pathprob / float(len(root.children))
+            #print '{0}Probpath: {1:.9f} Holes: {2} Board: {3} Children: {4}'.format('\t'*depth, pathprob, root.holecards, root.board, len(root.children))
+            for child in root.children:
+                subpayoffs = self.ev_helper(child, prob, depth+1)
+                for i,p in enumerate(subpayoffs):
+                    payoffs[i] += p
+            return payoffs
+        # Otherwise, it's an ActionNode
+        probs = self.strategies[root.player].probs(root.player_view)
+        #print '{0}Probpath: {1:.9f} PlayerView: {2} ActionProbs: {3}'.format('\t'*depth, pathprob, root.player_view, probs)
+        payoffs = [0] * self.gametree.players
+        if root.fold_action and probs[FOLD] > 0.0000000001:
+            subpayoffs = self.ev_helper(root.fold_action, pathprob * probs[FOLD], depth+1)
+            for i,p in enumerate(subpayoffs):
+                payoffs[i] += p
+        if root.call_action and probs[CALL] > 0.0000000001:
+            subpayoffs = self.ev_helper(root.call_action, pathprob * probs[CALL], depth+1)
+            for i,p in enumerate(subpayoffs):
+                payoffs[i] += p
+        if root.raise_action and probs[RAISE] > 0.0000000001:
+            subpayoffs = self.ev_helper(root.raise_action, pathprob * probs[RAISE], depth+1)
+            for i,p in enumerate(subpayoffs):
+                payoffs[i] += p
+        return payoffs
+
+
--- tests/test_strategy.py
+++ tests/test_strategy.py
@@ -4,32 +4,72 @@ sys.path.insert(0,os.path.realpath('.'))
 from pokerstrategy import *
 from pokergames import *
 
+hskuhn_gametree = half_street_kuhn()
+
+s0 = Strategy(0)
+s0.policy = { 'A:/:': [0,0,1], 'K:/:': [0,1,0], 'Q:/:': [0,2.0/3.0,1.0/3.0] }
+
+s1 = Strategy(1)
+s1.policy = { 'A:/r:': [0,1,0], 'K:/r:': [2.0/3.0,1.0/3.0,0], 'Q:/r:': [1,0,0], 'A:/c:': [0,1,0], 'K:/c:': [0,1,0], 'Q:/c:': [0,1,0] }
+
+eq0 = Strategy(0)
+eq0.build_default(hskuhn_gametree)
+eq0.save_to_file('tests/hskuhn_eq0.strat')
+
+eq1 = Strategy(1)
+eq1.build_default(hskuhn_gametree)
+eq1.save_to_file('tests/hskuhn_eq1.strat')
+
+print "Half-Street Kuhn"
+print "Holecards: {0}".format(hskuhn_gametree.deal_holecards())
+print "Nash0 vs. Nash1: {0}".format(StrategyProfile(hskuhn_gametree, [s0,s1]).expected_value())
+print "Nash0 vs. Eq1: {0}".format(StrategyProfile(hskuhn_gametree, [s0,eq1]).expected_value())
+print "Eq0 vs. Nash1: {0}".format(StrategyProfile(hskuhn_gametree, [eq0,s1]).expected_value())
+print "Eq0 vs. Eq1: {0}".format(StrategyProfile(hskuhn_gametree, [eq0,eq1]).expected_value())
+print ""
+
+leduc_gametree = leduc()
+
 s0 = Strategy(0)
 s0.load_from_file('strategies/leduc/0.strat')
 # Test a couple of arbitrary values
-assert(s0.probs('J:/:') == [0.072642889, 0.927357111, 0.000000000])
-assert(s0.probs('KJ:/rrc/rr:') == [0.000000000, 0.453178849, 0.546821151])
+assert(s0.probs('J:/:') == [0.000000000, 0.927357111, 0.072642889])
+assert(s0.probs('KJ:/rrc/rr:') == [0.546821151, 0.453178849, 0.000000000])
 # Verify we loaded all of infosets
 assert(len(s0.policy) == 144)
 
 s1 = Strategy(1)
 s1.load_from_file('strategies/leduc/1.strat')
 # Test a couple of arbitrary values
-assert(s1.probs('J:/r:') == [0.054870914, 0.125672407, 0.819456679])
-assert(s1.probs('KJ:/rrc/crr:') == [0.000000000, 0.999968742, 0.000031258])
+assert(s1.probs('J:/r:') == [0.819456679, 0.125672407, 0.054870914])
+assert(s1.probs('KJ:/rrc/crr:') == [0.000031258, 0.999968742, 0.000000000])
 # Verify we loaded all of infosets
 assert(len(s1.policy) == 144)
 
 # Generate a default strategy for player 0
-equal_probs0 = Strategy(0)
-equal_probs0.build_default(leduc())
-equal_probs0.save_to_file('tests/equal_probs0.strat')
-assert(len(equal_probs0.policy) == 144)
+eq0 = Strategy(0)
+eq0.build_default(leduc_gametree)
+eq0.save_to_file('tests/leduc_eq0.strat')
+assert(len(eq0.policy) == 144)
 
 # Generate a default strategy for player 1
-equal_probs1 = Strategy(1)
-equal_probs1.build_default(leduc())
-equal_probs1.save_to_file('tests/equal_probs1.strat')
-assert(len(equal_probs1.policy) == 144)
+eq1 = Strategy(1)
+eq1.build_default(leduc_gametree)
+eq1.save_to_file('tests/leduc_eq1.strat')
+assert(len(eq1.policy) == 144)
+
+rand0 = Strategy(0)
+rand0.load_from_file('strategies/leduc/random.strat')
+rand1 = Strategy(1)
+rand1.load_from_file('strategies/leduc/random.strat')
+
+print "Leduc"
+print "Nash0 vs. Nash1 EV: {0}".format(StrategyProfile(leduc_gametree, [s0,s1]).expected_value())
+print "Nash0 vs. Eq1 EV: {0}".format(StrategyProfile(leduc_gametree, [s0,eq1]).expected_value())
+print "Eq0 vs. Nash1 EV: {0}".format(StrategyProfile(leduc_gametree, [eq0,s1]).expected_value())
+print "Eq0 vs. Eq1: {0}".format(StrategyProfile(leduc_gametree, [eq0,eq1]).expected_value())
+print "Nash0 vs. Random: {0}".format(StrategyProfile(leduc_gametree, [s0,rand1]).expected_value())
+print "Random vs. Nash1: {0}".format(StrategyProfile(leduc_gametree, [rand0,s1]).expected_value())
+print ""
 
 print 'All passed!'
